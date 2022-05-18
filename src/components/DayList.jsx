b76f9307a5a8a92d097c60e3505ad7d21ce1ca59
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-17 20:05:41 -0500

    add setDay as a prop for DayListItem

--- src/components/DayList.jsx
+++ src/components/DayList.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import DayListItem from './DayListItem';
 
-const DayList = ({ days, day }) => {
+const DayList = ({ days, day, setDay }) => {
   return (
     <ul>
       {days.map((item) => (
-        <DayListItem key={item.id} name={item.name} spots={item.spots} selected={item.name === day} />
+        <DayListItem key={item.id} name={item.name} spots={item.spots} selected={item.name === day} setDay={setDay} />
       ))}
     </ul>
   );
