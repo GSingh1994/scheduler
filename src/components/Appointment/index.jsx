3b9aaa836e3edc5fd424260be8b9994c3d2497e2
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-18 21:59:28 -0500

    add props from stories to show single appointment component

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import './styles.scss';
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
 
-const Appointment = () => {
-  return <article className="appointment"></article>;
+const Appointment = ({ time, id, interview }) => {
+  return (
+    <article className="appointment">
+      <Header time={time} id={id} />
+      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+    </article>
+  );
 };
 
 export default Appointment;
