3a67ae933d7fb3a2f73e6cac12460250a101894d
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-18 19:45:27 -0500

    add stories for Status component

--- stories/index.js
+++ stories/index.js
@@ -12,6 +12,7 @@ import Header from 'components/Appointment/Header.jsx';
 import Empty from 'components/Appointment/Empty.jsx';
 import Show from 'components/Appointment/Show.jsx';
 import Confirm from 'components/Appointment/Confirm.jsx';
+import Status from 'components/Appointment/Status.jsx';
 
 storiesOf('Button', module)
   .addParameters({
@@ -107,5 +108,6 @@ storiesOf('Appointment', module)
   .add('Appointment with Time', () => <Appointment time={'12pm'} />)
   .add('Header', () => <Header time={'12pm'} />)
   .add('Empty', () => <Empty onAdd={action('onAdd')} />)
-  .add('Show', () => <Show student={'Lydia Miller-Jones'} interviewer={interviewers} onEdit={action('onEdit')} onDelete={action('onDelete')} />)
-  .add('Confirm', () => <Confirm message={'Delete the appointment?'} onConfirm={action('onConfirm')} onCancel={action('onCancel')} />);
+  .add('Show', () => <Show student={'Lydia Miller-Jones'} interviewer={interviewer} onEdit={action('onEdit')} onDelete={action('onDelete')} />)
+  .add('Confirm', () => <Confirm message={'Delete the appointment?'} onConfirm={action('onConfirm')} onCancel={action('onCancel')} />)
+  .add('Status', () => <Status message={'Deleting'} />);
