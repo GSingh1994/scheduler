9df9e55b769fa7b5ccfacda08e6fa8ff8475ffb8
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-19 18:58:44 -0500

    refactor to use axios to make GET request for days array

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -4,48 +4,54 @@ import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
 
-const appointments = {
-  1: {
-    id: 1,
-    time: '12pm',
-  },
-  2: {
-    id: 2,
-    time: '1pm',
-    interview: {
-      student: 'Lydia Miller-Jones',
-      interviewer: {
-        id: 3,
-        name: 'Sylvia Palmer',
-        avatar: 'https://i.imgur.com/LpaY82x.png',
-      },
-    },
-  },
-  3: {
-    id: 3,
-    time: '2pm',
-  },
-  4: {
-    id: 4,
-    time: '3pm',
-    interview: {
-      student: 'Archie Andrews',
-      interviewer: {
-        id: 4,
-        name: 'Cohana Roy',
-        avatar: 'https://i.imgur.com/FK8V841.jpg',
-      },
-    },
-  },
-  5: {
-    id: 5,
-    time: '4pm',
-  },
-};
+// const appointments = {
+//   1: {
+//     id: 1,
+//     time: '12pm',
+//   },
+//   2: {
+//     id: 2,
+//     time: '1pm',
+//     interview: {
+//       student: 'Lydia Miller-Jones',
+//       interviewer: {
+//         id: 3,
+//         name: 'Sylvia Palmer',
+//         avatar: 'https://i.imgur.com/LpaY82x.png',
+//       },
+//     },
+//   },
+//   3: {
+//     id: 3,
+//     time: '2pm',
+//   },
+//   4: {
+//     id: 4,
+//     time: '3pm',
+//     interview: {
+//       student: 'Archie Andrews',
+//       interviewer: {
+//         id: 4,
+//         name: 'Cohana Roy',
+//         avatar: 'https://i.imgur.com/FK8V841.jpg',
+//       },
+//     },
+//   },
+//   5: {
+//     id: 5,
+//     time: '4pm',
+//   },
+// };
 
 const Application = () => {
-  const [day, setDay] = useState('Monday');
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: 'Monday',
+    days: [],
+    appointments: {},
+  });
+
+  const setDay = (day) => setState((prev) => ({ ...prev, day }));
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
     axios.get('http://localhost:8001/api/days').then((response) => setDays(response.data));
@@ -57,16 +63,16 @@ const Application = () => {
         <img className="sidebar--centered" src="images/logo.png" alt="Interview Scheduler" />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} value={day} onChange={setDay} />
+          <DayList days={state.days} value={state.day} onChange={setDay} />
         </nav>
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {Object.values(appointments).map((appointment) => (
+        {Object.values(state.appointments).map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         {/* last appointment of the day */}
-        <Appointment key={Object.values(appointments).length + 1} time="5pm" />
+        <Appointment key={Object.values(state.appointments).length + 1} time="5pm" />
       </section>
     </main>
   );
