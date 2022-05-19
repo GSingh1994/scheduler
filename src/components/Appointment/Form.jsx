383226880862c1fc82a20f9d7e5d2b568511207f
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-18 21:34:40 -0500

    add controlled form and initiated state for student and interviewer

--- src/components/Appointment/Form.jsx
+++ src/components/Appointment/Form.jsx
@@ -1,31 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
-const Form = ({ student, interviewers, interviewer, onSave, onCancel }) => {
+const Form = (props) => {
+  const { interviewers, onSave, onCancel } = props;
+  const [student, setStudent] = useState(props.student || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setStudent('');
+    setInterviewer(null);
+  };
+  const cancel = () => {
+    onCancel();
+    reset();
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form onSubmit={(e) => e.preventDefault()} autoComplete="off">
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-          your code goes here
-        */
+            value={student}
+            onChange={(e) => setStudent(e.target.value)}
           />
         </form>
-        <InterviewerList interviewers={interviewers} />
+        <InterviewerList interviewers={interviewers} value={interviewer} onChange={setInterviewer} />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={onCancel} danger>
+          <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={onSave} confirm>
+          <Button onClick={() => onSave(student, interviewer)} confirm>
             Save
           </Button>
         </section>
