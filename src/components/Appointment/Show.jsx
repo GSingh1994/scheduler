8eb9ac61ec3ccaf9420183e31597b65e30fc175c
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-18 19:51:47 -0500

    fix props to the object interviewer

--- src/components/Appointment/Show.jsx
+++ src/components/Appointment/Show.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
-const Show = ({ student, onEdit, onDelete }) => {
+const Show = ({ student, onEdit, onDelete, interviewer }) => {
   return (
     <main className="appointment__card appointment__card--show">
       <section className="appointment__card-left">
         <h2 className="text--regular">{student}</h2>
         <section className="interviewer">
           <h4 className="text--light">Interviewer</h4>
-          <h3 className="text--regular">Sylvia Palmer</h3>
+          <h3 className="text--regular">{interviewer.name}</h3>
         </section>
       </section>
       <section className="appointment__card-right">
