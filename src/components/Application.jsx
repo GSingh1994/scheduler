278a13395d10695f39c2198d2329970d964a50e0
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-19 19:24:58 -0500

    add Promise.all to get days and appointment data from api

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,45 +3,7 @@ import axios from 'axios';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
-
-// const appointments = {
-//   1: {
-//     id: 1,
-//     time: '12pm',
-//   },
-//   2: {
-//     id: 2,
-//     time: '1pm',
-//     interview: {
-//       student: 'Lydia Miller-Jones',
-//       interviewer: {
-//         id: 3,
-//         name: 'Sylvia Palmer',
-//         avatar: 'https://i.imgur.com/LpaY82x.png',
-//       },
-//     },
-//   },
-//   3: {
-//     id: 3,
-//     time: '2pm',
-//   },
-//   4: {
-//     id: 4,
-//     time: '3pm',
-//     interview: {
-//       student: 'Archie Andrews',
-//       interviewer: {
-//         id: 4,
-//         name: 'Cohana Roy',
-//         avatar: 'https://i.imgur.com/FK8V841.jpg',
-//       },
-//     },
-//   },
-//   5: {
-//     id: 5,
-//     time: '4pm',
-//   },
-// };
+import getAppointmentsForDay from '../helpers/selectors';
 
 const Application = () => {
   const [state, setState] = useState({
@@ -50,11 +12,15 @@ const Application = () => {
     appointments: {},
   });
 
+  const dailyAppointments = getAppointmentsForDay(state, state.day);
+
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
-  const setDays = (days) => setState((prev) => ({ ...prev, days }));
 
   useEffect(() => {
-    axios.get('http://localhost:8001/api/days').then((response) => setDays(response.data));
+    Promise.all([axios.get('http://localhost:8001/api/days'), axios.get('http://localhost:8001/api/appointments')]).then((all) => {
+      console.log(all);
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+    });
   }, []);
 
   return (
@@ -68,11 +34,11 @@ const Application = () => {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {Object.values(state.appointments).map((appointment) => (
+        {dailyAppointments.map((appointment) => (
           <Appointment key={appointment.id} {...appointment} />
         ))}
         {/* last appointment of the day */}
-        <Appointment key={Object.values(state.appointments).length + 1} time="5pm" />
+        <Appointment key={dailyAppointments.length + 1} time="5pm" />
       </section>
     </main>
   );
