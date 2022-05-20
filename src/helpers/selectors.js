cd2beafc385eecaa7a84c41e79335f8f9f70ec08
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-19 19:23:36 -0500

    set getAppointmentsForDay as default export

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,4 +1,4 @@
-export function getAppointmentsForDay(state, day) {
+export default function getAppointmentsForDay(state, day) {
   const getTodaysAppointments = () => {
     const currentDay = state.days.filter((d) => d.name === day);
     //edge case
