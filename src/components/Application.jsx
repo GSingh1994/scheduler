03bd32accaddde0800ca0c00de4a2ba43c1e5023
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-20 12:16:01 -0500

    add interviewers to the state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -3,23 +3,33 @@ import axios from 'axios';
 import 'components/Application.scss';
 import DayList from './DayList';
 import Appointment from './Appointment';
-import getAppointmentsForDay from '../helpers/selectors';
+import { getAppointmentsForDay, getInterview } from '../helpers/selectors';
 
 const Application = () => {
   const [state, setState] = useState({
     day: 'Monday',
     days: [],
     appointments: {},
+    interviewers: {},
   });
 
   const dailyAppointments = getAppointmentsForDay(state, state.day);
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+
+    return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} />;
+  });
 
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
 
   useEffect(() => {
-    Promise.all([axios.get('http://localhost:8001/api/days'), axios.get('http://localhost:8001/api/appointments')]).then((all) => {
-      console.log(all);
-      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data }));
+    Promise.all([
+      axios.get('http://localhost:8001/api/days'),
+      axios.get('http://localhost:8001/api/appointments'),
+      axios.get('http://localhost:8001/api/interviewers'),
+    ]).then((all) => {
+      console.log(all[2].data);
+      setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
 
@@ -34,9 +44,7 @@ const Application = () => {
         <img className="sidebar__lhl sidebar--centered" src="images/lhl.png" alt="Lighthouse Labs" />
       </section>
       <section className="schedule">
-        {dailyAppointments.map((appointment) => (
-          <Appointment key={appointment.id} {...appointment} />
-        ))}
+        {schedule}
         {/* last appointment of the day */}
         <Appointment key={dailyAppointments.length + 1} time="5pm" />
       </section>
