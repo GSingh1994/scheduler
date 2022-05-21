dc998ee18afb299baa3f2857225f6cd90e189cfa
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-20 21:06:02 -0500

    add back function and tests for it

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,22 @@ import { useState } from 'react';
 
 const useVisualMode = (initial) => {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  const transition = (newMode) => {
+  const transition = (newMode, replace = false) => {
+    if (!replace) {
+      setHistory((prev) => prev.splice(-1, newMode));
+    }
     setMode(newMode);
+    setHistory((prev) => [...prev, mode]);
   };
 
-  return { mode, transition };
+  const back = () => {
+    const lastMode = history.pop();
+    setMode(lastMode);
+  };
+
+  return { mode, transition, back };
 };
 
 export default useVisualMode;
