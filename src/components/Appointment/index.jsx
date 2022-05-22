2a530f69cf7aacb53c22eec6e68351241d548afd
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-21 20:17:35 -0500

    add useVisualMode hook to Appointment component to transition between various modes

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -3,12 +3,22 @@ import './styles.scss';
 import Header from './Header';
 import Show from './Show';
 import Empty from './Empty';
+import Form from './Form';
+import useVisualMode from 'hooks/useVisualMode';
 
 const Appointment = ({ time, id, interview }) => {
+  const EMPTY = 'EMPTY';
+  const SHOW = 'SHOW';
+  const CREATE = 'CREATE';
+
+  const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
+
   return (
     <article className="appointment">
       <Header time={time} id={id} />
-      {interview ? <Show student={interview.student} interviewer={interview.interviewer} /> : <Empty />}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} />}
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === CREATE && <Form interviewers={[]} onCancel={back} />}
     </article>
   );
 };
