03df29ebce239075c30e1cdd3f006c88140c6cde
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-21 21:20:55 -0500

    add getInterviewersForDay function and it's tests

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -4,6 +4,12 @@ export function getAppointmentsForDay(state, day) {
   return currentDay.appointments.map((appointment) => state.appointments[appointment]);
 }
 
+export function getInterviewersForDay(state, day) {
+  const currentDay = state.days.find((d) => d.name === day);
+  if (!currentDay) return [];
+  return currentDay.interviewers.map((interviewer) => state.interviewers[interviewer]);
+}
+
 export function getInterview(state, interview) {
   if (!interview) return null;
   return { student: interview.student, interviewer: state.interviewers[interview.interviewer] };
