a92171f3c8437303fd2e306b939648e94e7d3c76
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-22 23:33:46 -0500

    add bookInterview function to update state upon adding new interview

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -13,12 +13,33 @@ const Application = () => {
     interviewers: {},
   });
 
+  const bookInterview = (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    setState((prev) => ({ ...prev, appointments }));
+  };
+
   const dailyAppointments = getAppointmentsForDay(state, state.day);
 
   const schedule = dailyAppointments.map((appointment) => {
     const interview = getInterview(state, appointment.interview);
     const interviewers = getInterviewersForDay(state, state.day);
-    return <Appointment key={appointment.id} id={appointment.id} time={appointment.time} interview={interview} interviewers={interviewers} />;
+    return (
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
+        interviewers={interviewers}
+        bookInterview={bookInterview}
+      />
+    );
   });
 
   const setDay = (day) => setState((prev) => ({ ...prev, day }));
@@ -29,7 +50,6 @@ const Application = () => {
       axios.get('http://localhost:8001/api/appointments'),
       axios.get('http://localhost:8001/api/interviewers'),
     ]).then((all) => {
-      console.log(all[2].data);
       setState((prev) => ({ ...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data }));
     });
   }, []);
