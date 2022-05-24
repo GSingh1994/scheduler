6ec54dc916b0b22824b7b9dc6e180915d76ef547
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-23 19:50:39 -0500

    add confirmation dialog before deleting an interview

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -5,6 +5,7 @@ import Show from './Show';
 import Empty from './Empty';
 import Form from './Form';
 import Status from './Status';
+import Confirm from './Confirm';
 import useVisualMode from 'hooks/useVisualMode';
 
 const Appointment = ({ time, id, interview, interviewers, bookInterview, cancelInterview }) => {
@@ -13,6 +14,7 @@ const Appointment = ({ time, id, interview, interviewers, bookInterview, cancelI
   const CREATE = 'CREATE';
   const SAVING = 'SAVING';
   const DELETING = 'DELETING';
+  const CONFIRM = 'CONFIRM';
 
   const { mode, transition, back } = useVisualMode(interview ? SHOW : EMPTY);
 
@@ -33,11 +35,12 @@ const Appointment = ({ time, id, interview, interviewers, bookInterview, cancelI
   return (
     <article className="appointment">
       <Header time={time} id={id} />
-      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={onDelete} />}
+      {mode === SHOW && <Show student={interview.student} interviewer={interview.interviewer} onDelete={() => transition(CONFIRM)} />}
       {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
       {mode === CREATE && <Form interviewers={interviewers} onCancel={back} onSave={save} />}
       {mode === SAVING && <Status message={'Saving'} />}
       {mode === DELETING && <Status message={'Deleting'} />}
+      {mode === CONFIRM && <Confirm message={'Deleting'} onConfirm={onDelete} />}
     </article>
   );
 };
