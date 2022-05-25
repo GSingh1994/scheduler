6377ec96345889b02fb289ddbad7c6a7b432a97f
GSingh1994 <singh.gagandeep0034@gmail.com>
2022-05-25 15:03:57 -0500

    add test to call onCancel and reset input field

--- src/components/__tests__/Form.test.js
+++ src/components/__tests__/Form.test.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { render, cleanup, fireEvent } from '@testing-library/react';
+import { fireEvent } from '@testing-library/react';
+
+import { render, cleanup } from '@testing-library/react';
 
 import Form from 'components/Appointment/Form';
 
@@ -21,29 +23,17 @@ describe('Form', () => {
   });
 
   it('renders with initial student name', () => {
-    const { getByTestId } = render(<Form interviewers={interviewers} student={'Lydia Miller-Jones'} />);
+    const { getByTestId } = render(<Form interviewers={interviewers} student="Lydia Miller-Jones" />);
     expect(getByTestId('student-name-input')).toHaveValue('Lydia Miller-Jones');
   });
 
   it('validates that the student name is not blank', () => {
     const onSave = jest.fn();
-
     const { getByText } = render(<Form interviewers={interviewers} onSave={onSave} />);
 
     fireEvent.click(getByText('Save'));
 
     expect(getByText(/student name cannot be blank/i)).toBeInTheDocument();
-    expect(onSave).not.toHaveBeenCalled();
-  });
-
-  it('validates that the interviewer cannot be null', () => {
-    const onSave = jest.fn();
-
-    const { getByText } = render(<Form interviewers={interviewers} student={'Lydia Miller-Jones'} onSave={onSave} />);
-
-    fireEvent.click(getByText('Save'));
-
-    expect(getByText(/please select an interviewer/i)).toBeInTheDocument();
     expect(onSave).not.toHaveBeenCalled();
   });
 
@@ -68,10 +58,28 @@ describe('Form', () => {
     expect(onSave).not.toHaveBeenCalled();
 
     fireEvent.click(getByAltText(interviewers[0].name));
-    // expect(queryByText(/Please select an interviewer/i)).toBeNull();
     fireEvent.click(getByText('Save'));
 
     expect(onSave).toHaveBeenCalledTimes(1);
     expect(onSave).toHaveBeenCalledWith('Lydia Miller-Jones', interviewers[0].id);
+  });
+
+  it('calls onCancel and resets the input field', () => {
+    const onCancel = jest.fn();
+    const { getByText, getByPlaceholderText, queryByText } = render(<Form interviewers={interviewers} student="Lydia Mill-Jones" onSave={jest.fn()} onCancel={onCancel} />);
+
+    fireEvent.click(getByText('Save'));
+
+    fireEvent.change(getByPlaceholderText('Enter Student Name'), {
+      target: { value: 'Lydia Miller-Jones' },
+    });
+
+    fireEvent.click(getByText('Cancel'));
+
+    expect(queryByText(/student name cannot be blank/i)).toBeNull();
+
+    expect(getByPlaceholderText('Enter Student Name')).toHaveValue('');
+
+    expect(onCancel).toHaveBeenCalledTimes(1);
   });
 });
